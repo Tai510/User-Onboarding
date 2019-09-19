3aa4d0af30a0a83426e9b6e968a162813251f4e0
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-09-19 15:22:17 -0700

    form validation n error messaging complete

--- user-onboarding/src/Component/UserForm.js
+++ user-onboarding/src/Component/UserForm.js
@@ -3,25 +3,68 @@ import { withFormik, Form, Field, Formik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function UserForm({values, handleChange}) {
+function UserForm({values, errors, touched}) {
   return (
       <div className ='userForm-Section'>
-            <form>
-                <input onChange={handleChange} value={values.name} name='name' placeholder='Name'></input>
-                <input onChange={handleChange} value={values.email} name='email' placeholder='Email'></input>
-                <input onChange={handleChange} value={values.password} name='password' placeholder='Password'></input>
-            </form>
+            <Form>
+
+                <div className='fullName'>
+                    <p>Full Name</p>
+                    <Field type='text' name='name'></Field>
+                </div> {/* fullName end */}
+                <div className='name-error'>
+                {touched.name && errors.name && (<p>{errors.name}</p>)}
+                </div>
+
+                <div className='email'>
+                    <p>Email</p>
+                    <Field type='email' name='email'></Field>
+                    {/* {touched.email && errors.email && (<p>{errors.email}</p>)} */}
+                </div> {/* email end */}
+                {touched.email && errors.email && (<p>{errors.email}</p>)}
+
+                <div className='password'>
+                    <p>Password</p>
+                    <Field type='password' name='password'></Field>
+                    {/* {touched.password && errors.password && (<p>{errors.password}</p>)} */}
+                </div> {/* password end */}
+                {touched.password && errors.password && (<p>{errors.password}</p>)}
+
+                <div className='checkbox-section'>
+                    <Field id='checkbox' name='agreedTerm' checked={values.agreedTerm} type='checkbox'>
+                    {/* <p>Agree to terms and services.</p> */}
+                    </Field>
+                    <p>Agree to terms and services.</p>
+                </div> {/* checkbox-section end */}
+
+                <button className='submit-button'>Submit</button>
+
+            </Form>
       </div> /* userForm-Section end */
   )
 }
 
 const FormikUserForm = withFormik({
-    mapPropsToValues() {
+    mapPropsToValues({name , email , password, agreedTerm}) {
         return {
-            name: 'Tai510',
-            email: 'tashiwoeser@gmail.com',
-            password: '**********'
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            agreedTerm: agreedTerm || false
         }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required() ,
+        email: Yup.string().required() ,
+        password: Yup.string().required() 
+    }),
+    handleSubmit(values) {
+        axios
+        .post(' https://reqres.in/api/users', values )
+        .then(res => {
+            console.log(res)
+        })
+        .catch()
     }
 })(UserForm)
 
